a151257e8e2d74d0ad68f8258ea7e0521e5f49ce
syed bilal <syed.bilal.bs@gmail.com>
2021-06-18 10:04:21 +0530

    Added Category Logic

--- my-app-api/app/controller/categoryController.js
+++ my-app-api/app/controller/categoryController.js
@@ -2,10 +2,18 @@
 
 const categoryDao = require('../models/dao/categoryDao');
 const productDao = require('../models/dao/productDao');
+const _ = require('lodash');
+const moment = require('moment');
+const axios = require('axios');
+axios.defaults.headers.common['Authorization'] = 'I3UE2JUA4U';
 let getCategory = async function (req, res) {
     try {
-
-     } catch (err) {
+        let result = await categoryDao.getAllCategory();
+        res.status(200).json({
+            status: "Success",
+            result: result
+        });
+    } catch (err) {
         console.log(err)
         res.status(400).json({
             status: "Failure"
@@ -16,6 +24,7 @@ let getCategory = async function (req, res) {
 
 let getCategoryById = async function (req, res) {
     try {
+        let result = await productDao.getProduct({ where: { subCategory: req.params.id } });
         res.status(200).json({
             status: "Success",
             result: result
